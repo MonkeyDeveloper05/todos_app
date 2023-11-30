755ca2bb8a561cb34b2618d73f7990b3f79e301b
Leo <leobado05gmail.com>
2023-11-30 16:30:52 +0100

    modified homeview

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+  type ToDo = {
+    id: number,
+    title: string,
+    completed: boolean,
+  } 
+
+  const list = ref<ToDo[]> ([
+    {
+      id: 1,
+      title: "ciao",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "hello",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "hola",
+      completed: true,
+    }
+])
+const newItem = ref('')
+function addOne() {
+list.value.push({
+  id: list.value.length + 1,
+  title: newItem.value,
+  completed: false,
+})
+}
+</script>
+
+<template>
+  <main class="container mt-4 bg-blue-500">
+    <form @submit.prevent="addOne">
+      <input type="text" v-model="newItem" placeholder="add title">
+      <button type="submit" @click="addOne">
+        add
+      </button>
+    </form>
+    <li v-for="item in list" :key="item.id">
+      {{ item.title }}
+    </li>
+  </main>
+</template>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,vue,ts}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem', 
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
